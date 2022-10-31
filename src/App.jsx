29ee355002f548a2862add346287bb1ebd5a7cd0
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-11-01 01:30:29 +0630

    font & logo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import AddForm from './components/AddForm';
 import ProductGroup from './components/ProductGroup';
-import logo from './images/brand-logo.jpg';
+import logo from './images/brand-logo.png';
 import { v4 as uuid } from 'uuid';
 import ProductRow from './components/ProductRow';
 
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <main className='bg-gray-100 h-screen'>
-      <div className='container h-100 flex bg-white mx-auto'>
+      <div className='container h-full flex bg-white mx-auto'>
         {/* add form */}
         <div className='w-1/2 p-4'>
           <AddForm newProduct={newProduct} setNewProduct={setNewProduct} />
@@ -23,8 +23,8 @@ function App() {
         {/* preview UI */}
         <div className='w-1/2 p-4 flex justify-center preview'>
           <div className='w-2/3 mx-auto'>
-            <div className='justify-center items-center'>
-              <img src={logo} alt='Logo' />
+            <div className='flex justify-center items-center'>
+              <img src={logo} alt='Logo' className='w-24 h-24' />
             </div>
             {/* Product group */}
             <ProductGroup />
