9463b27ce2feddab4012e1ba829af55e76582f2e
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-10-31 18:55:29 +0630

    pattern svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,50 @@
 function App() {
   return (
-    <h1 className='text-3xl font-bold text-blue-500 underline'>Hello world!</h1>
+    <main className='bg-gray-100 h-screen'>
+      <div className='container flex bg-white mx-auto'>
+        {/* add form */}
+        <div className='w-1/2 p-4'>
+          <form>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Product name</span>
+              </label>
+              <input
+                type='text'
+                placeholder='Type here'
+                className='input input-bordered input-sm w-full max-w-xs'
+              />
+            </div>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Price</span>
+              </label>
+              <input
+                type='text'
+                placeholder='Type here'
+                className='input input-bordered input-sm w-full max-w-xs'
+              />
+            </div>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Category</span>
+              </label>
+              <select className='select select-bordered select-sm w-full max-w-xs'>
+                <option disabled selected>
+                  Select category
+                </option>
+                <option>Drinks</option>
+                <option>Cake</option>
+                <option>Pizza</option>
+              </select>
+            </div>
+            <button className='btn btn-sm btn-primary mt-4'>Save</button>
+          </form>
+        </div>
+        {/* preview UI */}
+        <div className='w-1/2 p-4 preview'>preview</div>
+      </div>
+    </main>
   );
 }
 
